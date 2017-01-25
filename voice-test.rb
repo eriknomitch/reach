71fcdd5c4e11b1756552276d30d975eb3e5ce9a4
Erik Nomitch <enomitch@gmail.com>
2017-01-25 09:02:56 -0600

    Add sample script

--- voice-test.rb
+++ voice-test.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
 
+require "google/cloud/speech"
+
+speech = Google::Cloud::Speech.new
+
+audio = speech.audio "path/to/audio.raw",
+                       encoding: :raw, sample_rate: 16000
+results = audio.recognize
+
+result = results.first
+result.transcript #=> "how old is the Brooklyn Bridge"
+result.confidence #=> 0.9826789498329163
 
 
